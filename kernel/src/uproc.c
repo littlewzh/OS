152c0765273893c35698b81ad4cce95b381fa70e
tracer-nju <tracer@nju.edu.cn>
2022-06-27 00:11:17 +0800

    Linux little-bird 5.11.0-34-generic #36-Ubuntu SMP Thu Aug 26 19:22:09 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:11:17 up 10:14,  5 users,  load average: 0.86, 0.57, 0.64

--- kernel/src/uproc.c
+++ kernel/src/uproc.c
@@ -14,12 +14,12 @@ static int uproc_getpid(task_t *task){
     return 0;
 }
 static int uproc_sleep(task_t *task, int seconds){
-    printf("%d\n",seconds);
+    //printf("%d\n",seconds);
     uint64_t wakeup = io_read(AM_TIMER_UPTIME).us + 1000000L*seconds;
-    while(io_read(AM_TIMER_UPTIME).us < wakeup){
+    while(io_read(AM_TIMER_UPTIME).us < wakeup);//{
         //printf("check\n");
-        yield();
-    }
+       // yield();
+    //}
     return 0;
 }
 static int64_t uproc_uptime(task_t *task){
